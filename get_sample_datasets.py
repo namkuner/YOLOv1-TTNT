91375450a8e458eaee1ccdf50fe3ee88aa4902db
namkuner <namkuner@gmail.com>
2025-02-19 23:15:00 +0700

    check

--- get_sample_datasets.py
+++ get_sample_datasets.py
@@ -1,7 +1,7 @@
 import pandas as pd
-def read_csv(num_of_sample):
-    df = pd.read_csv("data/train.csv", nrows=num_of_sample)
-    df.to_csv(f"data/{num_of_sample}_examples.csv", index=False)
-
+def read_csv(type ="train",num_of_sample=1000):
+    df = pd.read_csv(f"data/{type}.csv", nrows=num_of_sample)
+    df.to_csv(f"data/{type}_{num_of_sample}_examples.csv", index=False)
+    return f"data/{type}_{num_of_sample}_examples.csv"
 if __name__ == "__main__":
     read_csv(1000)
